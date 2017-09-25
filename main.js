3d28b3a9f5f21e7de34a170e3deb6ca40840d1db
marbisman <maria.arbisman@gmail.com>
2017-09-24 19:33:08 -0700

    Added check for match within logic

--- main.js
+++ main.js
@@ -1,24 +1,25 @@
 var cards = ["queen", "queen", "king", "king"];
 
 var cardOne = cards[0];
-var cardTwo = cards[3];
+var cardTwo = cards[1];
 var cardThree = cards[2];
-var cardFour = cards[1];
-
+var cardFour = cards[3];
 var cardsInPlay = [];
 
-cardsInPlay.push(cardOne);
-
 console.log("User flipped " + cardOne);
+console.log("User flipped " + cardFour);
 
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
-
-if(cardsInplay.length === 2){
-  if(cardsInplay[0] === cardsInPlay[1]){
-    alert("You found a match!");
-  }
-  else{
-    alert("Sorry, try again");
-  }
+var checkForMatch = function (){
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+alert("You found a match!");
+} else {
+alert("Sorry, try again.");
+}
 };
+
+var flipCard = function(cardId){
+  checkForMatch();
+};
+
+flipCard(0);
+flipCard(2);
